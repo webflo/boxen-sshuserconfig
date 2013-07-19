63dc41071f9ba7074ef8b3a7c0ee479cabe4b9c1
em <eugen.mayer@kontextwork.de>
2013-07-19 13:59:40 +0200

    refactored, removed ssh path handling, using default. Now confing.d is use to store the config entrys. Remove _any_ handling of private files storage (wtf...)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,14 @@
-define sshuserconfig(
-  $ssh_config_dir = undef
-) {
-
+define sshuserconfig() {
   $unix_user = $title
-  if $ssh_config_dir == undef {
-    $ssh_config_dir_prefix = "/home/${unix_user}/.ssh"
-  } else {
-    $ssh_config_dir_prefix = $ssh_config_dir
-  }
+  $ssh_config_dir_prefix = "/home/${unix_user}/.ssh"
   $ssh_config_file = "${ssh_config_dir_prefix}/config"
-
   concat { $ssh_config_file :
     owner => $unix_user
+  }
+  
+  file { "$ssh_config_dir_prefix/config.d":
+    ensure => "directory",
+	mode => 0700
   }
 
 }
--- manifests/remotehost.pp
+++ manifests/remotehost.pp
@@ -2,27 +2,17 @@ define sshuserconfig::remotehost(
   $unix_user,
   $remote_hostname,
   $remote_username,
-  $private_key_content,
-  $public_key_content,
+  $priv_key_name = 'id_rsa',
   $remote_port = 22,
-  $ssh_config_dir = undef
 ) {
 
-  if $ssh_config_dir == undef {
-    $ssh_config_dir_prefix = "/home/${unix_user}/.ssh"
-  } else {
-    $ssh_config_dir_prefix = $ssh_config_dir
-  }
-
+  $ssh_config_dir_prefix = "/home/${unix_user}/.ssh"
   $ssh_config_file = "${ssh_config_dir_prefix}/config"
+  $ssh_config_folder = "${ssh_config_dir_prefix}/config.d"
+  $entry_name = "${title}"
+  $synthesized_privkey_path = "${ssh_config_dir_prefix}/$priv_key_name"
 
-  $concat_namespace = "ssh_userconfig_${unix_user}"
-  $fragment_name = "${concat_namespace}_${title}"
-
-  $synthesized_privkey_path = "${ssh_config_dir_prefix}/id_rsa_${title}"
-  $synthesized_pubkey_path = "${ssh_config_dir_prefix}/id_rsa_${title}.pub"
-
-  concat::fragment { $fragment_name :
+  file { "${$ssh_config_folder}/${$entry_name}" :
     target => $ssh_config_file,
     content => "Host ${title}
   HostName ${remote_hostname}
@@ -30,19 +20,4 @@ define sshuserconfig::remotehost(
   User ${remote_username}
   IdentityFile ${synthesized_privkey_path}\n\n"
   }
-
-  file { $synthesized_privkey_path :
-    ensure  => present,
-    content => $private_key_content,
-    owner   => $unix_user,
-    mode    => 600
-  }
-
-  file { $synthesized_pubkey_path :
-    ensure  => present,
-    content => $public_key_content,
-    owner   => $unix_user,
-    mode    => 600
-  }
-
 }
